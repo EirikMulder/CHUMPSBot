d2c93639e32bfd0542811be9b66c263244f4a4c9
Eirik Mulder <eirik.mulder@gmail.com>
2022-02-04 01:39:51 +0000

    init

---
+++ main.py
@@ -0,0 +1,107 @@
+import discord
+from time import sleep
+# import nacl # Voice
+
+from discord.ext import commands
+from collections import Counter
+
+
+bot = commands.Bot(command_prefix="$")
+people = {
+        "jack": 766415398530187305,
+        "karina": 533680717062733844,
+        "joey": 493254647780343808,
+        "eirik": 541302616068456448,
+}
+
+@bot.event
+async def on_ready():
+    global eirik
+    eirik = await bot.fetch_user(541302616068456448)
+    global poll_running, poll_votes, poll_options
+    poll_running = False
+    poll_votes = Counter()
+    poll_options = []
+
+@bot.event
+async def on_message(message):
+    if message.author != bot.user:
+        await eirik.send(message.author.name + ":")
+        await eirik.send(message.content)
+    await bot.process_commands(message)
+
+@bot.command(
+    help="Looks like you need some help.",
+    brief="Prints the list of values back to the channel."
+)
+async def say(ctx, *args):
+    response = ""
+
+    for arg in args:
+        response = response + " " + arg
+
+    await ctx.channel.send(response)
+
+@bot.command()
+async def hello(ctx, *args):
+    await ctx.channel.send("Hello noob!")
+    user = await bot.fetch_user(ctx.author.id)
+    await user.send("Hello! I'm CHUMPSBot!")
+
+@bot.command()
+async def msg(ctx, *args):
+    if args[0] in people:
+        user = await bot.fetch_user(people[args[0]])
+        print(user.name)
+        await eirik.send("messaging " + user.name + ": " + " ".join(args))
+        await user.send(" ".join(args))
+
+@bot.command()
+async def joinus(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+@bot.command()
+async def poll(ctx, *args):
+    # if poll_running:
+    #     print("There is already a poll running, use $stop_poll to stop it.")
+    global poll_running, poll_votes, poll_options
+
+    poll_running = True
+    poll_options = " ".join(args)
+    if "," in poll_options:
+        poll_options = poll_options.split(",")
+    else:
+        poll_options = poll_options.split(" ")
+    poll_options = [i.lower().strip() for i in poll_options]
+    if poll_options[-1].startswith("and"):
+        poll_options[-1] = poll_options[-1][3:].strip()
+    await ctx.channel.send(f"The poll_options are:\n"+ '\n'.join(poll_options))
+    await ctx.channel.send("Use $vote 'something' to vote. Use $stop_poll to stop the poll.")
+
+@bot.command()
+async def stop_poll(ctx):
+    global poll_running, poll_votes, poll_options
+    if not poll_running:
+        await ctx.channel.send("No poll is running. Use $poll 'options' to start one.")
+    await ctx.channel.send("The poll is finished!")
+    for option, votes in poll_votes.items():
+        await ctx.channel.send(f"{option} recieved {votes} votes!")
+    poll_running = False
+    poll_options = []
+    poll_votes = Counter()
+    
+@bot.command()
+async def vote(ctx, *args):
+    global poll_running, poll_votes, poll_options
+    if not poll_running:
+        await ctx.channel.send("No poll is active. Use $poll 'option1,option2' to start one")
+        return
+    pick = " ".join(args).strip().lower()
+    if pick in poll_options:
+        poll_votes[pick] += 1
+        await ctx.channel.send(f"Your vote for {pick} has been registered!")
+    else:
+        await ctx.channel.send("That is not one of the poll options.")
+
+bot.run("OTMyODM4MTkzNjk3NTIxNjY1.YeYzRA.ei7LevdTg802Krokzr024Q4zzKo")
